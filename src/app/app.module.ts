ff0bd0586f65dd7f7cde303b8fb99916b7bb5ec7
Yateesh-Hegde <yateeshhegde@gmail.com>
2020-10-10 18:46:51 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemsService } from './items.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, ItemsComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ItemsService]
 })
 export class AppModule { }
