81d64bad32b7338333053c738178236ba084d3cd
Yateesh-Hegde <yateeshhegde@gmail.com>
2020-10-10 18:48:11 +0530

    1 file added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ItemsComponent } from './items/items.component';
+
+const routes: Routes = [
+  {
+    path: 'items',
+    component: ItemsComponent
+  }
+];
